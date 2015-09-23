480d5ec3258d12376edfdb827283dd76e1d0f915
charush <nandhu@qburst.com>
2015-09-23 09:38:32 +0530

    updated student

--- src/main/java/com/qburst/sms/student/service/StudentService.java
+++ src/main/java/com/qburst/sms/student/service/StudentService.java
@@ -1,9 +1,20 @@
 package com.qburst.sms.student.service;
 
+import java.io.Serializable;
+
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Service;
 
+import com.qburst.sms.domain.Student;
 import com.qburst.swift.DAO.GenericCrudDAO;
+@SuppressWarnings("hiding")
 @Service
-public interface StudentService<T> extends GenericCrudDAO<T>{
+public interface StudentService<Student> extends GenericCrudDAO<Student>{
+	Student getProfileInfo(String username);
+	public void uploadImageOnId(String username,byte [] personImage);
 
+	public byte[] loadImage(Class<Student>clazz, Serializable pk);
+
+	
+	
 }
--- src/main/java/com/qburst/sms/student/service/StudentServiceImpl.java
+++ src/main/java/com/qburst/sms/student/service/StudentServiceImpl.java
@@ -1,11 +1,50 @@
 package com.qburst.sms.student.service;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+
 import org.springframework.stereotype.Component;
+
 import com.qburst.swift.DAO.GenericCrudDAOImpl;
+
+
+
 @Component
 @Transactional
-public class StudentServiceImpl<T> extends GenericCrudDAOImpl<T> implements StudentService<T>{
+public class StudentServiceImpl<Student> extends GenericCrudDAOImpl<Student> implements StudentService<Student>{
+
+	@SuppressWarnings("unchecked")
+	@Transactional
+	@Override
+	public Student getProfileInfo(String username) {
+
+	List<Student> students= (entityManager.createNamedQuery("Student.getProfileInfo")
+			.setParameter("username", username)
+			.getResultList());
+	if(students!=null && students.size()>0){
+		return students.get(0);
+	}
+	else{
+		return null;
+	}
+}
+
+@Override
+public byte[] loadImage(Class<Student> clazz, Serializable pk) {
+	Student t = findById(clazz, pk);
+	return ((com.qburst.sms.domain.Student) t).getImages();
+}
+
+@Override
+public void uploadImageOnId(String username,byte [] personImage) {
+        entityManager.createNamedQuery("Student.uploadImageOnId")
+                .setParameter("username", username).setParameter("personImage", personImage)
+                .executeUpdate();
+
+    }
+
 
 }
