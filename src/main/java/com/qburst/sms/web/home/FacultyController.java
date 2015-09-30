647ed8d19883124c3407cc926d027d2709421739
anjuaugustin <anjua@qburst.com>
2015-09-30 17:06:20 +0530

    faculty controller having faculty details update and student list

--- src/main/java/com/qburst/sms/web/home/FacultyController.java
+++ src/main/java/com/qburst/sms/web/home/FacultyController.java
@@ -6,17 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.security.Principal;
 
 import com.qburst.sms.domain.Faculty;
+import com.qburst.sms.domain.Student;
 import com.qburst.sms.faculty.service.FacultyService;
+import com.qburst.sms.student.service.StudentService;
 
 @Controller
 public class FacultyController {
 	@Autowired
 	private FacultyService<Faculty> facultyService;
+	
+	@Autowired
+	private StudentService<Student> studentService;
 	
 	@RequestMapping(value = "/faculty",method=RequestMethod.GET)
 	public String facultyhome(Model model,Principal principal) {
@@ -28,18 +34,27 @@ public class FacultyController {
 		}
 		List<?> listUser = facultyService.list(Faculty.class);
 		model.addAttribute("persons", listUser);
+		List<?> listUser1 = studentService.list(Student.class);
+		model.addAttribute("students", listUser1);
 		return "faculty/facultyPage";
 	}
 	
-	@RequestMapping(value="/faculty/facultyEdit",method=RequestMethod.POST)
-	public String facultyEdit(@ModelAttribute Faculty faculty,Principal principal){
-		if(faculty.id == 0){
+	@RequestMapping(value="/faculty",method = RequestMethod.POST)
+	public ModelAndView facultyEdit(@ModelAttribute Faculty faculty,Principal principal){
+		Faculty faculty1=facultyService.getSearchedFaculty(faculty.getFacultyName());
+		System.out.println("id1="+principal.getName());
+		if(faculty1 == null){
 			facultyService.save(faculty);
 		}else{
-		//facultyService.saveById(Faculty.class,faculty.id);
-		facultyService.deleteById(Faculty.class,faculty.id);
-		facultyService.save(faculty);
+			facultyService.update(faculty);	
 		}
-		return "redirect:/faculty";
+		ModelAndView modelView = new ModelAndView("faculty/facultyPage");
+		modelView.addObject("faculty",faculty);
+		List<?> listUser = facultyService.list(Faculty.class);
+		modelView.addObject("persons", listUser);
+		List<?> listUser1 = studentService.list(Student.class);
+		modelView.addObject("students", listUser1);
+		
+		return modelView;
 	}
 }
